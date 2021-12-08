cb6dc9397af95fd3a0cf33fdd4177ee330e24a56
Matthew Bess <mcbess11@gmail.com>
2021-12-08 15:55:48 -0500

    CreateGame component and view created

---
+++ vue/src/components/CreateGame.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+      <div class="nav">
+          <nav-bar/>
+      </div>
+      <div class="create-game-container">
+          <form action="">
+              <label for="game-name">Name: </label>
+              <input type="text" class="text-input" name="game-name">
+              <input type="number" >
+          </form>
+      </div>
+  </div>
+</template>
+
+<script>
+import NavBar from './NavBar.vue'
+export default {
+    components: { NavBar }
+}
+</script>
+
+<style>
+
+</style>
