ba309adbcd7bd30bcbb1408c08549e743c214b7e
Eric Catalan <ericcatalan52@gmail.com>
2021-12-09 12:30:57 -0500

    pushing updated usertables view

--- vue/src/views/ViewGames.vue
+++ vue/src/views/ViewGames.vue
@@ -3,10 +3,18 @@
       <div class="nav">
           <nav-bar/>
       </div>
-      <div v-for="userGame in games" :key="userGame.id">
-          <h3>{{userGame.gameName}}</h3>
-          <h3>{{userGame.startingAmount}}</h3>
-      </div>
+      <table class = "userGames" >
+          <tr class = "game-header">
+              <th>Game Name</th>
+              <th>Starting Amount</th>
+              <th>End Date</th>
+          </tr>
+          <tr class = "game-info" v-for="userGame in games" :key="userGame.id">
+            <td class="game-data">{{userGame.gameName}}</td>
+            <td class="game-data">{{userGame.startingAmount}}</td> 
+            <td class="game-data">{{userGame.endDate}}</td>
+          </tr>
+      </table>
   </div>
 </template>
 
@@ -26,17 +34,59 @@ export default {
             }
         }
     },
-    
     created() {
         gameService.viewGames().then(response=>{
             this.games=response;})
        
     }
-
-    
 }
 </script>
 
 <style>
+.container{
+    display: grid;
+    grid-template-columns:1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 2fr 1fr;
+    grid-template-areas: 
+    "nav . . . . ."
+    "nav . table table table ."
+    "nav . . . . .";
 
+}
+.nav{
+    grid-area: nav;
+}
+.userGames {
+    display: flex;
+    flex-direction: column;
+    
+border:1px solid black;
+grid-area: table;
+
+  
+}
+.game-header{
+  display:flex;
+
+	background: red;
+    justify-content: space-evenly;
+}
+
+.game-info{
+    display:flex;
+	
+    justify-content: space-around;
+
+}
+
+
+.game-data{
+    display: flex;
+    justify-content: space-evenly;
+}
+
+th,td{
+    padding: 20px;
+    
+}
 </style>
