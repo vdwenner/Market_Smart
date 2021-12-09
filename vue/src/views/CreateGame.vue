e0125771ce76774c777658bb749d6d2518ec27bc
Matthew Bess <mcbess11@gmail.com>
2021-12-08 21:01:51 -0500

    Create game feature working

--- vue/src/views/CreateGame.vue
+++ vue/src/views/CreateGame.vue
@@ -1,19 +1,47 @@
 <template>
   <div class="container">
-      <create-game-form/>
+      <div class="nav">
+          <nav-bar/>
+      </div>
+      <div class="create-game-container">
+          <form @submit.prevent="createGame">
+              <label for="game-name">Name: </label>
+              <input type="text" class="text-input" name="game-name" v-model="game.gameName">
+              <label for="starting-amount">Starting Cash: </label>
+              <select name="starting-amount" id="starting-amount" v-model.number="game.startingAmount">
+                  <option value="10000" class="amount">10,000</option>
+                  <option value="25000" class="amount">25,000</option>
+                  <option value="50000" class="amount">50,000</option>
+                  <option value="100000" class="amount">100,000</option>
+              </select>
+              <label for="end-date">Ending Date: </label>
+              <input type="date" name="end-date" min="date" id="date" v-model="game.endDate">
+              <button class="btn btn-lg btn-primary btn-block" type="submit">Create Game</button>
+          </form>
+      </div>
   </div>
 </template>
 
 <script>
-import CreateGameForm from "../components/CreateGameForm.vue";
+import NavBar from "../components/NavBar"
 import gameService from "../services/GameService";
 
 export default {
-  components: { CreateGameForm },
+  components: { NavBar },
+    data() {
+        return {
+            date: new Date().toISOString().slice(0, 10),
+        game: {
+            gameName: '',
+            startingAmount: '',
+            endDate: ''
+          }
+        }
+    },
   methods: {
     createGame() {
       gameService.createGame(this.game).then((response) => {
-        if(response.status == 201) {
+        if(response.status == 200) {
           this.$router.push({
             path: '/',
           })
@@ -26,5 +54,12 @@ export default {
 </script>
 
 <style>
+  .container {
+        display: flex;
+    }
+    
+    .create-game-container {
+        display: flex;
 
+    }
 </style>
